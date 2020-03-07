05292f4104af1f11ac61777f3173cf0b70f67eba
Winter Jung <wintermy201@gmail.com>
2020-03-08 01:03:27 +0900

    Implement split action (#1)
    * Init action metadata
    
    * Implement split
    
    * Add ci actions
    
    * Define numeric outputs
    
    * Attach prefix _

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from typing import Optional
+
+
+def set_action_output(name: str, value: str):
+    print(f'::set-output name={name}::{value}')
+
+
+def print_action_error(msg: str):
+    print(f'::error file={__name__}::{msg}')
+
+
+def get_action_input(
+    name: str, required: bool = False, default: Optional[str] = None
+) -> str:
+    v = os.environ.get(f'INPUT_{name.upper()}', '')
+    if v == '' and default:
+        v = default
+    if required and v == '':
+        print_action_error(f'input required and not supplied: {name}')
+        exit(1)
+    return v
+
+
+def main():
+    msg = get_action_input('msg', required=True)
+    seperator = get_action_input('seperator', required=False, default=' ')
+    maxsplit = int(get_action_input('maxsplit', required=False, default='-1'))
+
+    results = msg.split(sep=seperator, maxsplit=maxsplit)
+
+    for i, result in enumerate(results):
+        set_action_output('_' + str(i), result)
+    set_action_output('length', len(results))
+
+
+if __name__ == '__main__':
+    main()
