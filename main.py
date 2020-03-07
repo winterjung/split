cea820c5324cd958f99e595d2e6a5f138227f5c7
jungwinter <wintermy201@gmail.com>
2020-03-08 01:17:42 +0900

    Limit maxsplit

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from typing import Optional
 
 
+DEFINED_ACTION_OUTPUTS_NUMBER = 100
+
+
 def set_action_output(name: str, value: str):
     print(f'::set-output name={name}::{value}')
 
@@ -28,6 +31,8 @@ def main():
     maxsplit = int(get_action_input('maxsplit', required=False, default='-1'))
 
     results = msg.split(sep=seperator, maxsplit=maxsplit)
+    if len(results) > DEFINED_ACTION_OUTPUTS_NUMBER:
+        results = msg.split(sep=seperator, maxsplit=DEFINED_ACTION_OUTPUTS_NUMBER - 1)
 
     for i, result in enumerate(results):
         set_action_output('_' + str(i), result)
